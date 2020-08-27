4ab3d444e87d2aee00ed30be46452ae97ffac1f3
tonysun03 <tonysun03@163.com>
2020-08-27 21:38:08 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'tonysun的博客',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+          { text: '社会人', link: '/' },
+          { text: '小佩奇', link: '/guide/' },
+          { text: '我是百度', link: 'https://baidu.com' },
+        ],
+        sidebar: [
+            '/a/',
+            '/guide/',
+            '/b/'
+          ]
+      }
+  }
